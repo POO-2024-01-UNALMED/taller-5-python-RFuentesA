cdaddee69cb358d2cbb2e3c0e94e7204ef0c6483
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-03-18 18:48:22 -0500

    Update zoologico.py

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,11 +1,27 @@
 class Zoologico:
 
     def __init__(self, nombre, ubicacion, zonas = None):
+
         self._nombre = nombre
         self._ubicacion = ubicacion
         self._zonas = zonas or []
+    
+    def cantidadTotalAnimales(self):
+        
+        if self._zonas != None:
 
-    def agregarzonas(self, zona = None):
+            total_animales = 0
+
+            for zona in self._zonas:
+
+                total_animales += zona.cantidadAnimales()
+            
+            
+            return total_animales
+
+    
+    def agregarZonas(self, zona = None):
+
         if zona != None:
             self._zonas.append(zona)
 
@@ -14,14 +30,3 @@ class Zoologico:
     
     def getNombre(self):
         return self._nombre
-
-
-    def cantidadTotalAnimales(self):
-        
-        if self._zonas != None:
-            total_animales = 0
-            for zona in self._zonas:
-                total_animales += zona.cantidadAnimales()
-
-        return total_animales
-    
